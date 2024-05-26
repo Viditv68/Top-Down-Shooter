bc3f6bbcdea992cbdcc92b9502d8c0ff4ba8f6b7
DESKTOP-7FM26BG\CrimEMasterGoGo <viditrawatv68@gmail.com>
2024-05-26 08:41:13 +0530

    added models

--- Assets/Scripts/Player/Target.cs
+++ Assets/Scripts/Player/Target.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Target : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.layer = LayerMask.NameToLayer("Enemy");
     }
 
     // Update is called once per frame
